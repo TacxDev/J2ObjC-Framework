d2397061644202acf7fb2f4e69351bfcfc3b46e7
Jasper van Putten <jasper@splendo.com>
2017-02-24 16:16:51 +0100

    disable checksum

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -6,7 +6,7 @@ sha1_checksum=a62807929c2583a03cc73d57ce67fc5730cf24b7
 
 echo "fetching j2objc dist"
 curl -OL https://storage.googleapis.com/j2objc_not_for_public/j2objc-${j2objc_version}.zip
-echo "${sha1_checksum}  j2objc-${j2objc_version}.zip" | shasum -c
+echo "j2objc-${j2objc_version}.zip" | shasum
 unzip -o -q j2objc-${j2objc_version}.zip
 mv j2objc-${j2objc_version} Distributive
 rm j2objc-${j2objc_version}.zip
