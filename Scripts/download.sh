5482404a38dad5f248b36b31d616c0a3a7842c69
Jasper van Putten <jasper@splendo.com>
2017-02-24 16:08:00 +0100

    change link

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -5,7 +5,7 @@ j2objc_version=1.3.1
 sha1_checksum=a62807929c2583a03cc73d57ce67fc5730cf24b7
 
 echo "fetching j2objc dist"
-curl -OL https://github.com/google/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
+curl -OL https://storage.googleapis.com/j2objc_not_for_public/j2objc-${j2objc_version}.zip
 echo "${sha1_checksum}  j2objc-${j2objc_version}.zip" | shasum -c
 unzip -o -q j2objc-${j2objc_version}.zip
 mv j2objc-${j2objc_version} Distributive
