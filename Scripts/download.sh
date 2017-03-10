9636ea7b241455f5d12c9ea113643ed0cd8f7956
Jasper van Putten <jasper@splendo.com>
2017-03-10 13:00:46 +0100

    improve download script

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -6,14 +6,59 @@ sha1_checksum=a62807929c2583a03cc73d57ce67fc5730cf24b7
 
 echo "fetching j2objc dist"
 echo "fetching j2objc"
-for i in {1..5}
+  for i in {1..5}
     do
-        curl -O https://storage.googleapis.com/j2objc_not_for_public/j2objc-${j2objc_version}.zip
+        curl --range 0-199999999 -o j2objc-${j2objc_version}.zip.part1 https://storage.googleapis.com/j2objc_not_for_public/j2objc-${j2objc_version}.zip
         status=$?
         if [ "$status" -eq 0 ]; then
             break
         fi
     done
+    for i in {1..5}
+    do
+        curl --range 200000000-399999999 -o j2objc-${j2objc_version}.zip.part2 https://storage.googleapis.com/j2objc_not_for_public/j2objc-${j2objc_version}.zip
+        status=$?
+        if [ "$status" -eq 0 ]; then
+            break
+        fi
+    done
+    for i in {1..5}
+    do
+       curl --range 400000000-599999999 -o j2objc-${j2objc_version}.zip.part3 https://storage.googleapis.com/j2objc_not_for_public/j2objc-${j2objc_version}.zip
+       status=$?
+       if [ "$status" -eq 0 ]; then
+          break
+       fi
+    done
+    for i in {1..5}
+    do
+       curl --range 600000000-799999999 -o j2objc-${j2objc_version}.zip.part4 https://storage.googleapis.com/j2objc_not_for_public/j2objc-${j2objc_version}.zip
+       status=$?
+       if [ "$status" -eq 0 ]; then
+          break
+       fi
+    done
+    for i in {1..5}
+    do
+       curl --range 800000000-999999999 -o j2objc-${j2objc_version}.zip.part5 https://storage.googleapis.com/j2objc_not_for_public/j2objc-${j2objc_version}.zip
+       status=$?
+       if [ "$status" -eq 0 ]; then
+          break
+       fi
+    done
+    for i in {1..5}
+    do
+       curl --range 1000000000- -o j2objc-${j2objc_version}.zip.part6 https://storage.googleapis.com/j2objc_not_for_public/j2objc-${j2objc_version}.zip
+       status=$?
+       if [ "$status" -eq 0 ]; then
+          break
+       fi
+    done
+
+cat j2objc-${j2objc_version}.zip.part? > j2objc-${j2objc_version}.zip
+rm j2objc-${j2objc_version}.zip.part?
+
+
 echo "j2objc-${j2objc_version}.zip" | shasum
 unzip -o -q j2objc-${j2objc_version}.zip
 mv j2objc-${j2objc_version} Distributive
