1c31a26f6c1359dbe9175ccef84d0c62b463651e
Jasper van Putten <jasper@tacx.com>
2023-11-30 11:48:25 +0100

    raise version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 private let github = "https://github.com"
 private let repo = "TacxDev/J2ObjC-Framework"
-private let version = "2.8"
+private let version = "2.8.2"
 
 private let baseLink = "\(github)/\(repo)/releases/download/\(version)/"
 private let jreLink = "\(baseLink)JRE.xcframework.zip"
